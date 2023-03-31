11628e4b89b69b2011de24c2b515726dee29024d
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-03-31 19:11:55 +0200

    wip(4): end ex

--- src/main/scala/u04lab/extractor/extractor.scala
+++ src/main/scala/u04lab/extractor/extractor.scala
@@ -9,9 +9,33 @@ object List:
 
   def cons[E](h: E, t: List[E]): List[E] = Cons(h, t)
 
-  def append[A](left: List[A], right: List[A]): List[A] = left match
+  def append[E](left: List[E], right: List[E]): List[E] = left match
     case Cons(head, rest) => Cons(head, append(rest, right))
     case Nil() => right
+  @scala.annotation.tailrec
+  def foreach[E](l: List[E])(op: E => Unit): Unit = l match
+    case Cons(h, t) => op(h); foreach(t)(op)
+    case Nil() =>
+  def length(list: List[_]): Int = List.sum(List.map(list)(_ => 1))
+  def sum(l: List[Int]): Int = l match
+    case Cons(h, t) => h + sum(t)
+    case _ => 0
+  def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
+    case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+    case Nil() => Nil()
+  def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+    case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+    case Cons(_, t) => filter(t)(pred)
+    case Nil() => Nil()
+  import u04lab.code.Option
+  import u04lab.code.Option.*
+  def find[A](list: List[A])(f: A => Boolean): Option[A] = list match
+    case Cons(elem, rest) if f(elem) => Some(elem)
+    case Cons(elem, rest) => find(rest)(f)
+    case _ => None()
+  def contains[A](list: List[A], elem: A): Boolean = !Option.isEmpty(find(list)(_ == elem))
+
+
 
   // Factory with varargs
   def apply[E](args: E*): List[E] =
@@ -19,4 +43,33 @@ object List:
     for e <- args do list = append(list, cons(e, Nil()))
     list
 
-  
+
+import u04lab.extractor.List.*
+trait Item:
+  def code: Int
+  def name: String
+  def tags: List[String]
+
+
+object Item:
+  def apply(code: Int, name: String, tags: String*): Item =
+    var l: List[String] = List.Nil()
+    for tag <- tags do l = List.append(l, List.Cons(tag, Nil()))
+    ItemImpl(code, name, l)
+
+  private case class ItemImpl(code: Int, name: String, tags: List[String]) extends Item
+
+
+// Extractor
+object sameTag:
+  def unapply(l: List[Item]): Option[List[String]] = l match
+    case Cons(h, t) =>
+      var commonTags: List[String] = Nil()
+      foreach(h.tags)(tag => { if all(t)(tag) then commonTags = append(commonTags, cons(tag, Nil())) })
+      commonTags match
+        case Cons(_, _) => Some(commonTags)
+        case _ => None
+    case Nil() => None
+
+  private def all(list: List[Item])(tag: String): Boolean =
+    length(list) == length(filter(list)(item => contains(item.tags, tag)))
--- test/scala/u04lab/extractor/ExtractorTest.scala
+++ test/scala/u04lab/extractor/ExtractorTest.scala
@@ -11,4 +11,19 @@ class ExtractorTest:
 
   @Test def testListCreation() =
     assertEquals(Nil(), List())
-    assertEquals(Cons(1, Cons(2, Cons(3, Nil()))), List(1, 2, 3))
+//    assertEquals(Cons(1, Cons(2, Cons(3, Nil()))), List(1, 2, 3))⇒
+
+  val dellXps = Item(33, "Dell XPS 15", "notebook", "pc")
+  val dellInspiron = Item(34, "Dell Inspiron 13", "notebook", "pc")
+  val xiaomiMoped = Item(35, "Xiaomi S1", "moped", "mobility")
+  @Test def testExtractorNoMatches() =
+    val noMatches: Option[List[String]] = List(dellXps, xiaomiMoped) match
+      case sameTag(t) => Some(t)
+      case _ => None
+    assertEquals(None, noMatches)
+
+  @Test def testExtractorWithMatches() =
+    val matches: Option[List[String]] = List(dellXps, dellInspiron) match
+      case sameTag(t) => Some(t)
+      case _ => None
+    assertEquals(Some(List("notebook", "pc")), matches)
