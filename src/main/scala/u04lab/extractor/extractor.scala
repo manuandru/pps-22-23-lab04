bd34f29193a70f991d5333bb192d6a59e39854af
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-03-31 17:54:53 +0200

    wip(4): factory with varargs

--- src/main/scala/u04lab/extractor/extractor.scala
+++ src/main/scala/u04lab/extractor/extractor.scala
@@ -9,4 +9,14 @@ object List:
 
   def cons[E](h: E, t: List[E]): List[E] = Cons(h, t)
 
-  def apply[E](args: E*): List[E] = Nil()
+  def append[A](left: List[A], right: List[A]): List[A] = left match
+    case Cons(head, rest) => Cons(head, append(rest, right))
+    case Nil() => right
+
+  // Factory with varargs
+  def apply[E](args: E*): List[E] =
+    var list: List[E] = Nil()
+    for e <- args do list = append(list, cons(e, Nil()))
+    list
+
+  
